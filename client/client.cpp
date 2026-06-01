ff9659e406ee2a764d007f2737a4c15ce1dc5ec2
Huzaifa-Jamil <huzaifajamil.ke24@gmail.com>
2026-06-01 06:40:24 +0500

    add version 2 of client.exe

--- client/client.cpp
+++ client/client.cpp
@@ -1,29 +1,59 @@
 #include <QCoreApplication>
 #include <QTcpSocket>
+#include <QTextStream>
+#include <QThread>
 #include <QDebug>
+#include <QMetaObject>
+
+QTcpSocket socket;
+
+void inputLoop()
+{
+    QTextStream in(stdin);
+
+    while (true)
+    {
+        QString msg = in.readLine();
+
+        if (msg == "/exit")
+            break;
+
+        qDebug().noquote() << "[You]:" << msg;
+
+        QMetaObject::invokeMethod(&socket, [msg]()
+        {
+            socket.write(msg.toUtf8() + "\n");
+            socket.flush(); }, Qt::QueuedConnection);
+    }
+}
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    QTcpSocket socket;
-
     socket.connectToHost("127.0.0.1", 8080);
-    qDebug() << "Client Starts";
 
-    if (socket.waitForConnected(10000))
+    QObject::connect(&socket, &QTcpSocket::connected, []()
+    { 
+        qDebug() << "Connected to server";
+    });
+
+    QObject::connect(&socket, &QTcpSocket::readyRead, [&]()
     {
-        qDebug() << "Connected to server !! ";
+        while (socket.canReadLine())
+        {
+            QString msg = socket.readLine().trimmed();
+            qDebug().noquote() << "[Other]:" << msg;
+        }
+    });
 
-        socket.write("Hello Server !!");
-        socket.waitForReadyRead();
-
-        qDebug() << "server Says : " << socket.readAll();
-    }
-    else
+    QObject::connect(&socket, &QTcpSocket::disconnected, []()
     {
-        qDebug() << "Timeout for connection !! bad day ";
-    }
+        qDebug() << "[Server]: Disconnected";
+    });
 
-    return 0;
+    QThread *t = QThread::create(inputLoop);
+    t->start();
+
+    return a.exec();
 }
